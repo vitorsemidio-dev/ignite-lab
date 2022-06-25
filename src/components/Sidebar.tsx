f7e85531f48934edaaf47cba897a628c732bfdc3
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-25 19:44:36 -0300

    Conclusão aula 05
    Geração graphql com codegen
    Refatoração componentes Sidebar, Subscribe, Video

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,8 @@
-import { gql, useQuery } from '@apollo/client';
+import { useGetLessonsQuery } from '../graphql/generated';
 import { Lesson } from './Lesson';
 
-const GET_LESSON_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      slug
-      title
-      availableAt
-    }
-  }
-`;
-
-interface GetLessonQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: 'live' | 'class';
-  }[];
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonQueryResponse>(GET_LESSON_QUERY);
+  const { data } = useGetLessonsQuery();
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,5 @@
+import '@vime/core/themes/default.css';
+
 import { DefaultUi, Player, Youtube } from '@vime/react';
 import {
   CaretRight,
@@ -6,49 +8,20 @@ import {
   Lightning,
 } from 'phosphor-react';
 
-import '@vime/core/themes/default.css';
-import { gql, useQuery } from '@apollo/client';
+import { useGetLessonBySlugQueryQuery } from '../graphql/generated';
 
 interface VideoProps {
   lessonSlug: string;
 }
 
-const GET_LESSON_BY_SLUG_SLUG = gql`
-  query GetQueryBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      lessonType
-      title
-      videoId
-      teacher {
-        name
-        bio
-        avatarURL
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-}
-
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_SLUG, {
+  const { data } = useGetLessonBySlugQueryQuery({
     variables: {
       slug: props.lessonSlug,
     },
   });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return <p className="flex-1">Carregando</p>;
   }
 
@@ -68,24 +41,27 @@ export function Video(props: VideoProps) {
           <div className="flex-1">
             <h1 className="text-2xl font-bold">{data?.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              {data?.lesson.description}
+              {data?.lesson?.description}
             </p>
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data?.lesson.teacher.avatarURL}
-                alt="Professor do curso"
-              />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">
-                  {data?.lesson.teacher.name}
-                </strong>
-                <span className="text-gray-200 text-sm block">
-                  {data?.lesson.teacher.bio}
-                </span>
+            {data?.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data?.lesson.teacher.avatarURL}
+                  alt="Professor do curso"
+                />
+
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data?.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data?.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
