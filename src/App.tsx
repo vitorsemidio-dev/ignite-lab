d943accf7f430d935c90dcf4de94530512e1d36e
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-25 07:35:04 -0300

    Conclusão aula 2
    Criação componente sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-        bio
-      }
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-  teacher?: Teacher;
-}
-
-interface Teacher {
-  name: string;
-  bio: string;
-  lessons: Lesson[];
-}
+import { Event } from './pages/Event';
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  console.log(data);
   return (
-    <div>
-      <h1 className="tailwind-with-class">Hello World Ignite Lab</h1>
-      <ul>
-        {data?.lessons.map((lesson) => {
-          return <li key={lesson.id}>{lesson.title}</li>;
-        })}
-      </ul>
-    </div>
+    <>
+      <Event />
+    </>
   );
 }
 
