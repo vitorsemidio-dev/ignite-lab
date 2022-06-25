f959a8822a9b16400fc070072810889e35c55e1b
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-25 16:37:23 -0300

    Conclusão aula 3
    Criação rotas das páginas
    Implementação Player de vídeo
    Upgrade versão node

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,5 @@
 import { CheckCircle, Lock } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -21,10 +22,10 @@ export function Lesson(props: LessonProps) {
   );
 
   return (
-    <a href="">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-cemter justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -44,6 +45,6 @@ export function Lesson(props: LessonProps) {
         </header>
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
